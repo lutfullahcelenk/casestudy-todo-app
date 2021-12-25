eeb9cfc893972141dd7009326725e195fdcf1a78
Lütfullah ÇELENK <>
2021-12-26 00:06:52 +0300

    input section is styled

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -14,17 +14,28 @@ export const StyledForm = styled.form`
 
 export const StyledInput = styled.input`
   padding: 14px 32px 14px 16px;
+  border-radius: 4px 0 0 4px;
+  border: 2px solid #5d0cff;
+  border-radius: 10px;
+  outline: none;
+  width: 70%;
+  background: #00bd94;
   color: #fff;
-  background: transparent;
+
+  &::placeholder {
+    color: #e2e2e2;
+    font-size: 1rem;
+  }
 `;
 
 export const StyledButton = styled.button`
   padding: 16px;
   border: none;
   border-radius: 0 4px 4px 0;
+  border-radius: 10px;
   cursor: pointer;
   outline: none;
-  background-color: #5d0cff; 
+  background-color: #5d0cff;
   color: white;
   text-transform: capitalize;
 `;
--- src/components/ToDo/styles.js
+++ src/components/ToDo/styles.js
@@ -10,6 +10,9 @@ export const TodoContainer = styled.div`
 export const TodoTitle = styled.h1`
   margin: 32px 0;
   color: #fff;
-  font-size: 2.5rem;
-  text-align: center
+  font-size: 2rem;
+  text-align: center;
+  background-color: #800080;
+  padding: 1rem;
+  border-radius: 10px;
 `
