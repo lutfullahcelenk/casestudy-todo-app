91b2e26503a42d221d618a8bdd45fadb6bc85071
Lütfullah ÇELENK <>
2021-12-25 23:58:29 +0300

    react router dom işs installed and router structure is set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import "./App.css";
-
+import { BrowserRouter as Router, Route , Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-      <Home />
-      <About />
-      <Contact />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
